fe411588fa01bf0b7ab7d0c772c5a529b7c76240
bettercodevivek <viveksingh0520@gmail.com>
2024-06-02 21:51:32 +0530

    Updation

--- LHD/src/Navbar/Navbar.jsx
+++ LHD/src/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = ()=>{
   return(
-   <nav id='navbar' class='bg-orange-600 flex flex-1'>
+   <nav id='navbar' class='bg-orange-600 flex flex-1 justify-between'>
     <div>
       home
     </div>
