272d87a5afd0139aa39f1c60d710fd41ac20fe79
bettercodevivek <viveksingh0520@gmail.com>
2024-06-02 21:42:40 +0530

    updation

--- LHD/src/App.jsx
+++ LHD/src/App.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './Navbar/Navbar'
 
-const Home = () => <div className="p-4">Home Page</div>;
-const About = () => <div className="p-4">About Page</div>;
-const Services = () => <div className="p-4">Services Page</div>;
-const Contact = () => <div className="p-4">Contact Page</div>;
+import Navbar from './Navbar/Navbar'
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/services" component={Services} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
-    </Router>
+    <Navbar />
   );
 }
 
--- LHD/src/Navbar/Navbar.jsx
+++ LHD/src/Navbar/Navbar.jsx
@@ -1,59 +1,15 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-
-const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto flex items-center justify-between">
-        <div className="text-white text-2xl">
-          <Link to="/">Logo</Link>
-        </div>
-        <div className="md:hidden">
-          <button onClick={() => setIsOpen(!isOpen)} className="text-white focus:outline-none">
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              {isOpen ? (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              ) : (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16m-7 6h7"
-                />
-              )}
-            </svg>
-          </button>
-        </div>
-        <div className={`md:flex ${isOpen ? 'block' : 'hidden'}`}>
-          <Link to="/" className="block mt-4 md:inline-block md:mt-0 text-white mr-4">
-            Home
-          </Link>
-          <Link to="/about" className="block mt-4 md:inline-block md:mt-0 text-white mr-4">
-            About
-          </Link>
-          <Link to="/services" className="block mt-4 md:inline-block md:mt-0 text-white mr-4">
-            Services
-          </Link>
-          <Link to="/contact" className="block mt-4 md:inline-block md:mt-0 text-white">
-            Contact
-          </Link>
-        </div>
-      </div>
-    </nav>
+const Navbar = ()=>{
+  return(
+   <nav id='navbar' class='bg-orange-600 flex-1'>
+    <a href="#">home</a>
+    <a href="#">home</a>
+    <a href="#">home</a>
+    <a href="#">home</a>
+    <a href="#">home</a>
+    <a href="#">home</a>
+    <a href="#">home</a>
+   </nav>
   );
-};
+}
 
 export default Navbar;
