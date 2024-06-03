ce393b8688b8b069c5c5d9ec716cb7c40adf3f4f
bettercodevivek <viveksingh0520@gmail.com>
2024-06-03 22:57:05 +0530

    Updation

--- LHD/src/Hero/Hero.jsx
+++ LHD/src/Hero/Hero.jsx
@@ -1,8 +1,11 @@
+
 const Hero = () =>{
     return(
         <div>
             <img class='h-dvh w-11/12 relative left-16 top-12 z-0 opacity-80 blur-0 rounded-3xl' src="https://i.postimg.cc/cCtYdKYJ/pexels-shvetsa-3683074.jpg"></img>
-            <div></div>
+            <div>
+                
+            </div>
         </div>
     );
 }
