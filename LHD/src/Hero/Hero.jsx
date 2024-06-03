a8d2266426f599240502ebaddd4738d70877cd99
bettercodevivek <viveksingh0520@gmail.com>
2024-06-03 20:28:08 +0530

    updation

--- LHD/src/Hero/Hero.jsx
+++ LHD/src/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () =>{
     return(
         <div>
-            <img class='h-dvh w-dvw relative -top-24 z-0 opacity-80 blur-0' src="https://i.postimg.cc/cCtYdKYJ/pexels-shvetsa-3683074.jpg"></img>
+            <img class='h-dvh w-dvw relative -top-16 z-0 opacity-80 blur-0' src="https://i.postimg.cc/cCtYdKYJ/pexels-shvetsa-3683074.jpg"></img>
         </div>
     );
 }
--- LHD/src/Navbar/Navbar.jsx
+++ LHD/src/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
 const Navbar = ()=>{
   return(
-   <nav id='navbar' class=' flex flex-1 justify-between h-24 items-center z-10 relative shadow-md'>
+   <nav id='navbar' class=' flex flex-1 justify-between h-16 items-center z-10 relative shadow-md'>
     <div class='flex items-center text-2xl mx-2'>
-      <img class='h-24 rounded-full aspect-square' src="src/assets/LHD HUMAN CARE logo.png"></img>
+      <img class='h-16 rounded-full aspect-square' src="src/assets/LHD HUMAN CARE logo.png"></img>
       <div class='flex-row flex-1 mx-2'>
        <div> LHD Human Care </div>
         <div class='text-base'>Crafting Health,Shaping Future</div>
