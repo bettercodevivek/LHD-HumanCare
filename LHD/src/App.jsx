c3fc060001c9a9528bab9c67bf2a3e6e776bb7ee
bettercodevivek <viveksingh0520@gmail.com>
2024-06-03 23:37:21 +0530

    updation

--- LHD/src/App.jsx
+++ LHD/src/App.jsx
@@ -5,7 +5,7 @@ import Hero from './Hero/Hero';
 
 function App() {
   return (
-    <div class='bg-white text-black font-serif antialiased tracking-wide'>
+    <div class='bg-white text-black font-serif antialiased tracking-wide overflow-x-hidden overflow-y-hidden'>
     <Navbar />
     <Hero />
     </div>
--- LHD/src/Hero/Hero.jsx
+++ LHD/src/Hero/Hero.jsx
@@ -3,8 +3,16 @@ const Hero = () =>{
     return(
         <div>
             <img class='h-dvh w-11/12 relative left-16 top-12 z-0 opacity-80 blur-0 rounded-3xl' src="https://i.postimg.cc/cCtYdKYJ/pexels-shvetsa-3683074.jpg"></img>
-            <div>
-                
+            <div class='relative border-black shadow-lg flex flex-row flex-1 items-center h-32 w-4/5 left-36 -top-4 rounded-3xl justify-around'>
+                <div class=''>
+                   <span class='font-bold text-4xl'>28</span>  <br></br>
+                    Years of Experience
+                </div>
+                <div class=''>
+                   <span class='font-bold text-4xl'> 5000+</span> <br/>
+                    Active Clients</div>
+                <div class=''><span class='font-bold text-4xl'>All North Indian</span> <br/>
+                States Covered</div>
             </div>
         </div>
     );
