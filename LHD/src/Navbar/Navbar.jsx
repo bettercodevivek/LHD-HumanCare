5ccba0c82809bc72c0486c5e4d478f2920667f7a
bettercodevivek <viveksingh0520@gmail.com>
2024-06-03 23:54:22 +0530

    updation

--- LHD/src/Navbar/Navbar.jsx
+++ LHD/src/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = ()=>{
   return(
-   <nav id='navbar' class=' flex flex-1 justify-between h-16 items-center z-10 relative top-4 left-16'>
+   <nav id='navbar' class=' flex flex-1 justify-between h-20 w-dvw items-center z-10 fixed top-0 left-16 bg-white'>
     <div class='flex items-center text-2xl mx-2'>
       <img class='h-16 rounded-full aspect-square' src="src/assets/LHD HUMAN CARE logo.png"></img>
       <div class='flex-row flex-1 mx-2'>
