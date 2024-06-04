20b55b5db24440303b9941c0c6107c1444145046
bettercodevivek <viveksingh0520@gmail.com>
2024-06-04 14:34:13 +0530

    file Addition

--- LHD/src/Hero/Hero.jsx
+++ LHD/src/Hero/Hero.jsx
@@ -2,8 +2,8 @@ const Hero = () =>{
     return(
         <div>
             <img class='h-dvh w-11/12 relative left-16 top-24 z-0 opacity-80 blur-0 rounded-3xl' src="https://i.postimg.cc/cCtYdKYJ/pexels-shvetsa-3683074.jpg"></img>
-            <div>
-                <video src=""></video>
+            <div class='relative'>
+                
             </div>
             <div class='relative bg-inherit backdrop-blur-lg shadow-lg flex flex-row flex-1 items-center h-44 w-4/5 left-36 -top-0 rounded-3xl justify-around border-2'>
                 <div class=''>
