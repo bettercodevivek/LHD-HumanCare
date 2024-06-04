e44a35df3d15823df199e4c2f0cf36caefbe48ab
bettercodevivek <viveksingh0520@gmail.com>
2024-06-04 14:50:51 +0530

    Updation

--- LHD/src/Hero/Hero.jsx
+++ LHD/src/Hero/Hero.jsx
@@ -3,7 +3,7 @@ const Hero = () =>{
         <div>
             <img class='h-dvh w-11/12 relative left-16 top-24 z-0 opacity-80 blur-0 rounded-3xl' src="https://i.postimg.cc/cCtYdKYJ/pexels-shvetsa-3683074.jpg"></img>
             <div class='relative'>
-                
+                <p>LHD Human Care</p>
             </div>
             <div class='relative bg-inherit backdrop-blur-lg shadow-lg flex flex-row flex-1 items-center h-44 w-4/5 left-36 -top-0 rounded-3xl justify-around border-2'>
                 <div class=''>
