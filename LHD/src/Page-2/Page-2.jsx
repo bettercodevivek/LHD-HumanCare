9a0c40b208fd5f027f1dcb3e5449e378a84512fd
bettercodevivek <viveksingh0520@gmail.com>
2024-06-04 22:27:58 +0530

    updation

--- LHD/src/Page-2/Page-2.jsx
+++ LHD/src/Page-2/Page-2.jsx
@@ -4,8 +4,8 @@ const Page2=()=>{
 return(
  <div>
     <h1 class='text-4xl text-center font-bold'> Why choose LHD Human Care ? </h1>
-    <p>
-        We believe in serving the best pharmaceutical products at affordable prices.
+    <p class='text-2xl'>
+        We believe in serving the best pharmaceutical products at affordable prices.<br/>
         Quality over everything is what we strive for.
     </p>
  </div>
