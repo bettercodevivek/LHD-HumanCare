804a7d009501968b6e851bcc44cb72f2de9886a9
bettercodevivek <viveksingh0520@gmail.com>
2024-06-04 22:34:58 +0530

    updation

--- LHD/src/Page-2/Page-2.jsx
+++ LHD/src/Page-2/Page-2.jsx
@@ -4,9 +4,12 @@ const Page2=()=>{
 return(
  <div>
     <h1 class='text-4xl text-center font-bold'> Why choose LHD Human Care ? </h1>
-    <p class='text-2xl'>
+    <p class='text-lg text-center my-2 text-zinc-800'>
         We believe in serving the best pharmaceutical products at affordable prices.<br/>
-        Quality over everything is what we strive for.
+        Quality over everything is what we strive for.<br />
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br/>
+        Quidem doloribus dolorem deleniti? Quisquam cum est dolorum enim dolore repellendus doloribus,<br/>
+         impedit qui culpa praesentium temporibus at. Ducimus qui facilis veritatis.
     </p>
  </div>
 );
