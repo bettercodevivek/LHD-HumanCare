1ec6c802762c26aef678fd1c3306cff052dca2a3
bettercodevivek <viveksingh0520@gmail.com>
2024-06-06 13:43:18 +0530

    Addition of new file

---
+++ LHD/src/ImageSlide/ImageSlide.jsx
@@ -0,0 +1,7 @@
+const ImageSlide = [
+    {url:'https://i.postimg.cc/cCtYdKYJ/pexels-shvetsa-3683074.jpg'},
+    {url:'https://i.postimg.cc/3JRk6SbC/walter-otto-PT70-CT6m-ATQ-unsplash.jpg'},
+    {url:'https://i.postimg.cc/cCtYdKYJ/pexels-shvetsa-3683074.jpg'}
+]
+
+export default ImageSlide;
