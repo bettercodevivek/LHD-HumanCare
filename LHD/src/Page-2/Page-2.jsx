5763fd5a6fdd292c7bfda2dd5d1ac89ad06158ea
bettercodevivek <viveksingh0520@gmail.com>
2024-06-08 10:26:15 +0530

    updation

--- LHD/src/Page-2/Page-2.jsx
+++ LHD/src/Page-2/Page-2.jsx
@@ -19,24 +19,31 @@ const Page2=()=>{
       };
 
 return(
- <div className="relative top-12">
+ <div className="h-screen relative top-12">
     <h1 class='text-4xl text-center font-bold'> Why choose LHD Human Care ? </h1>
-    <div className="flex flex-row  min-h-screen bg-white relative top-16">
-      <div className="w-full sm:w-1/2 lg:w-1/3 p-4">
+    <div className=" grid grid-cols-2 gap-8 bg-white relative top-16">
+      <div className=" ">
         <Card 
           title="Strong API Legacy" 
           description="With over 28 years of expertise, our robust API portfolio stands as a testament to our commitment to quality and innovation. Trusted by healthcare providers and pharmaceutical companies worldwide, we deliver consistent, high-quality Active Pharmaceutical Ingredients that form the foundation of effective and reliable medications. Explore our legacy of excellence and discover why we are a leader in the pharmaceutical industry." 
           imageUrl="src/assets/card1-icon.svg" 
         />
       </div>
-      <div className="w-full max-h-min sm:w-1/2 lg:w-1/3 p-4">
+      <div className=" ">
         <Card 
           title="Our Vision" 
           description="At LHD Human Care, we envision a world where advanced science and compassionate care come together to create innovative solutions that improve the quality of life for everyone. Our commitment to pioneering research, sustainable practices, and equitable healthcare drives us to break boundaries and set new standards in the pharma industry. We aspire to be a trusted leader, empowering healthier lives" 
           imageUrl="https://via.placeholder.com/150" 
         />
       </div>
-      <div className="w-full sm:w-1/2 lg:w-1/3 p-4">
+      <div className=" ">
+        <Card 
+          title="Kind Care Initiative" 
+          description="At LHD Human Care, we envision a world where advanced science and compassionate care come together to create innovative solutions that improve the quality of life for everyone. Our commitment to pioneering research, sustainable practices, and equitable healthcare drives us to break boundaries and set new standards in the pharma industry. We aspire to be a trusted leader, empowering healthier lives"
+          imageUrl="https://via.placeholder.com/150" 
+        />
+      </div>
+      <div className=" ">
         <Card 
           title="Kind Care Initiative" 
           description="At LHD Human Care, we envision a world where advanced science and compassionate care come together to create innovative solutions that improve the quality of life for everyone. Our commitment to pioneering research, sustainable practices, and equitable healthcare drives us to break boundaries and set new standards in the pharma industry. We aspire to be a trusted leader, empowering healthier lives"
