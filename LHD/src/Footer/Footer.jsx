8f2f39581d1db7ee3224278c725b6a71983eed82
bettercodevivek <viveksingh0520@gmail.com>
2024-06-08 22:06:57 +0530

    updation

--- LHD/src/Footer/Footer.jsx
+++ LHD/src/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer=()=>{
     return(
     <div className="relative top-12 h-dvh bg-blue-500">
-        
+        <iframe className="w-1/2 h-1/2" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d448193.951046296!2d76.76356261822156!3d28.644287349748502!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x37205b715389640!2sDelhi!5e0!3m2!1sen!2sin!4v1717864213431!5m2!1sen!2sin" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
     );
 }
