a248a1290c7be2bee913270fe92292303a9aa80f
bettercodevivek <viveksingh0520@gmail.com>
2024-06-09 20:19:53 +0530

    Updation

--- LHD/src/Footer/Footer.jsx
+++ LHD/src/Footer/Footer.jsx
@@ -1,7 +1,9 @@
 const Footer = () =>{
     return(
     <div className='relative top-4 h-dvw w-dvw bg-sky-100'>
-        
+        <div>
+            <img src='src/assets/LHD Human Care Logo.png'></img>
+        </div>
     </div>
     );
 }
