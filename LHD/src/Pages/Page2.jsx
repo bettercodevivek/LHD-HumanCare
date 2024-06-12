fc92d21442b894d9f4e0b90f5212b129a7d0acd0
bettercodevivek <viveksingh0520@gmail.com>
2024-06-12 20:47:42 +0530

    Updation

--- LHD/src/Pages/Page2.jsx
+++ LHD/src/Pages/Page2.jsx
@@ -26,7 +26,7 @@ const Page2 = () => {
         <div>
           <Card
             title={'Strong API Legacy'}
-            description={'With over 28 years of expertise, our robust API portfolio stands as a testament to our commitment to quality and innovation. Trusted by healthcare providers and pharmaceutical companies worldwide, we deliver consistent, high-quality Active Pharmaceutical Ingredients that form the foundation of effective and reliable medications. Explore our legacy of excellence and discover why we are a leader in the pharmaceutical industry.'}
+            description={'With over 28 years of expertise, our robust API portfolio stands as a testament to our commitment to quality and innovation. Trusted by healthcare providers and pharmaceutical companies worldwide, we deliver consistent, high-quality Active Pharmaceutical Ingredients that form the foundation of effective and reliable medications. Explore our legacy of excellence and discover why we are a leader in the pharmaceutical industry. Explore our legacy of excellence.'}
             imageUrl={'src/assets/card1-icon.svg'}
           />
         </div>
@@ -42,7 +42,7 @@ const Page2 = () => {
         <div>
           <Card
             title={'"Health for all" initiative'}
-            description={'Through our "Health for All" initiative, we are committed to making healthcare accessible to underserved communities. We conduct free medical checkup camps where our dedicated team of doctors provides comprehensive health assessments. During these camps, we also distribute our medicines at no cost, ensuring that those in need receive the essential care and treatments they deserve. Our goal is to bridge the gap in healthcare access.'}
+            description={'Through our "Health for All" initiative, we are committed to making healthcare accessible to underserved communities. We conduct free medical checkup camps where our dedicated team of doctors provides comprehensive health assessments. During these camps, we also distribute our medicines at no cost, ensuring that those in need receive the essential care and treatments they deserve.'}
             imageUrl={'src/assets/holding-hands-in-a-circle-svgrepo-com.svg'}
           />
         </div>
