bc8215668bf1c72d73eb6913507f0385e8a8139b
bettercodevivek <viveksingh0520@gmail.com>
2024-06-19 18:31:15 +0530

    Updation in codebase

--- LHD/src/Components/Home.jsx
+++ LHD/src/Components/Home.jsx
@@ -3,12 +3,14 @@ import Hero from '../Pages/Hero';
 import Page2 from '../Pages/Page2';
 import Page3 from '../Pages/Page3';
 import Page4 from '../Pages/Page4';
+import Hospital from '../Pages/Hospital';
 const Home = () => {
   return (
     <>
     <Hero/>
     <Page2/>
     <Page3/>
+    <Hospital/>
     <Page4/>
     </>
   );
