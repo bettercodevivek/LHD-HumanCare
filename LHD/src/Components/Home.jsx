b57def3d694e5daa2de51cd30858e4ec074750cc
bettercodevivek <viveksingh0520@gmail.com>
2024-07-10 15:07:45 +0530

    updation

--- LHD/src/Components/Home.jsx
+++ LHD/src/Components/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Hero from '../Pages/Hero';
 import Page2 from '../Pages/Page2';
 import Page3 from '../Pages/Page3';
-import Page4 from '../Pages/Page4';
 import Hospital from '../Pages/Hospital';
 const Home = () => {
   return (
